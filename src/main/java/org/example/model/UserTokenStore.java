36a4ea9595aa41679e016d1be10d3034322618ec
DANI3L008 <dani3l009@outlook.com>
2025-11-14 17:55:17 -0600

    calendar

---
+++ src/main/java/org/example/model/UserTokenStore.java
@@ -0,0 +1,17 @@
+package org.example.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserTokenStore {
+
+    private final Map<String, UserToken> store = new HashMap<>();
+
+    public void save(UserToken token) {
+        store.put(token.getPhone(), token);
+    }
+
+    public UserToken find(String phone) {
+        return store.get(phone);
+    }
+}
